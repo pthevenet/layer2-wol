3eff6a7874ecbb7a4fb46322d0703a802dcc669b
Pierre Thévenet <thevenet.pierre@gmail.com>
2018-02-04 13:33:40 +0100

    layer2-wol

--- webserver/public/script.js
+++
@@ -1,7 +0,0 @@
-// onoff sends a get request to /on if type=on, to /off otherwise
-function onoff(type) {
-  console.log(type)
-  var xhttp = new XMLHttpRequest();
-  xhttp.open("GET", type);
-  xhttp.send()
-}
